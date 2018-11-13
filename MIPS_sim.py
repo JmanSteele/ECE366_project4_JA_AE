c96d4c416b20ae27fcefec212aa7ed6f5b6c6c75
Justino Almazan <jalmaz9@uic.edu>
2018-11-13 14:26:32 -0600

    Updated MIPS_sim.py, still need to debug, added cycles

--- MIPS_sim.py
+++ MIPS_sim.py
@@ -1,3 +1,5 @@
+#Justino Almazan
+#Alberto Espinoza
 print("ECE 366 Project 4 MIPS simulator")
 #file_reader reads each line of a file in an array as elements
 #input: file -- read MIPS instruction
@@ -6,7 +8,7 @@ def file_to_array(file):
     return_array = []
     #read in each line into array elements and ignore newlines
     for line in file:
-        line = line.partition('#')[0]
+        line=line.partition('#')[0]
         line=line.rstrip()
         if line[0:2] == "0x":
             return_array.append(line)
@@ -20,6 +22,10 @@ def execute_operation(op, reg_arr, pc, branch):
         rt=int(op[14:20], 2)
         reg_arr[rd]=int(reg_arr[rs]) + int(reg_arr[rt])
         pc+=4
+        cycle[0]+=1
+        cycle[1]+=4
+        cycle[2]+=1
+        cycle[4]+=1
     elif op[2:8]=="001000":
         #addi instruction
         print("ADDI")
@@ -27,6 +33,10 @@ def execute_operation(op, reg_arr, pc, branch):
         imm = int(op[20:34], 2)
         reg_arr[rt]=int(reg_arr[rs]) + imm
         pc+=4
+        cycle[0]+=1
+        cycle[1]+=4
+        cycle[2]+=1
+        cycle[4]+=1
     elif op[2:8]=="000000":
         print("SUB")
         #sub instruction
@@ -34,6 +44,10 @@ def execute_operation(op, reg_arr, pc, branch):
         rt=int(op[14:20], 2)
         reg_arr[rd]=int(reg_arr[rs]) - int(reg_arr[rt])
         pc+=4
+        cycle[0]+=1
+        cycle[1]+=4
+        cycle[2]+=1
+        cycle[4]+=1
     elif op[2:8]=="000000" and op[29:34]=="100110":
         print("XOR")
         #xor instruction
@@ -42,31 +56,49 @@ def execute_operation(op, reg_arr, pc, branch):
         rd=int(op[20:24], 2)
         reg_arr[rd]=int(reg_arr[rs]) ^ int(reg_arr[rt])
         pc+=4
+        cycle[0]+=1
+        cycle[1]+=4
+        cycle[2]+=1
+        cycle[4]+=1
     elif op[2:8]=="000100":
         print("BEQ")
         #Branch instruction
         rs=int(op[8:14], 2)
         rt=int(op[14:20], 2)
         offset=(op[20:34], 2)
+        cycle[0]+=1
+        cycle[1]+=3
+        cycle[3]+=1
         if reg_arr[rs]==reg_arr[rt]:
             pc+=offset*4
+            cycle[2]+=2
         else:
             pc+=4
+            cycle[2]+=1
     elif op[2:8]=="000101":
         print("BNE")
         #branch if not equal to instruction
         rs=int(op[8:14], 2)
         rt=int(op[14:20], 2)
+        cycle[0]+=1
+        cycle[1]+=3
+        cycle[3]+=1
         if reg_arr[rs]!=reg_arr[rt]:
             pc+=offset*4
+            cycle[2]+=2
         else:
             pc+=4
+            cycle[2]+=1
     elif op[2:8]=="000000" and op[20:34]=="101010":
         print("SLT")
         #set less than
         rs=int(op[8:14], 2)
         rt=int(op[14:20], 2)
         rd=int(op[20:24], 2)
+        cycle[0]+=1
+        cycle[1]+=4
+        cycle[2]+=1
+        cycle[4]+=1
         if reg_arr[rs]<reg_arr[rt]:
             reg_arr[rd]=1
         else:
@@ -81,6 +113,10 @@ def execute_operation(op, reg_arr, pc, branch):
         reg_arr[rs]+=offset    #move register across memory by offset to access value
         reg_arr[rt]=reg_arr[rs]#set register rt equal to whatever value rs obtains from memorys
         pc+=4
+        cycle[0]+=1
+        cycle[1]+=5
+        cycle[2]+=2
+        cycle[5]+=1
     elif op[2:8]=="101011":
         print("SW")
         #store word
@@ -90,13 +126,18 @@ def execute_operation(op, reg_arr, pc, branch):
         reg_arr[rs]+=offset
         reg_arr[rs]=reg_arr[rt]
         pc+=4
+        cycle[0]+=1
+        cycle[1]+=4
+        cycle[2]+=1
+        cycle[4]+=1
 #sim: simulates the MIPS hex code
 #inputs: file name of txt that carries the instructions
 def sim (MIPS_HEX):
     #initialize pc and register array
     pc = 0
     branch = 0
-    reg_arr = [0, 0, 0, 0, 0, 0, 0] #registers [$1, $2, $3, $4, $5, $6, $7]
+    reg_arr = [0, 0, 0, 0, 0, 0, 0, 0] #registers [$0, $1, $2, $3, $4, $5, $6, $7]
+    cycle=[0, 0, 0, 0, 0, 0] #this array will work like so [single cycle, multi cycle, pipeline, 3cycle, 4cycle, 5cycle]
     #create file variables from file name strings
     instr_mem_input = open(MIPS_HEX, "r")#read file for programming instructions
     instr_mem = file_to_array(instr_mem_input)
@@ -104,11 +145,18 @@ def sim (MIPS_HEX):
     while pc < len(instr_mem):
         op = instr_mem[pc]
         print("PC: ", pc)
-        data_set = execute_operation(op, reg_arr, pc, branch)
+        data_set = execute_operation(op, reg_arr, pc, branch, cycle)
         reg_arr = data_set[0]
         pc=data_set[1]
         branch=data_set[2]
+        cycle=data_set[3]
         print("register array:", reg_arr)#prints for each instruction so we can see what is being stored in each register
         dic+=1 #increment DIC by 1 everytime we perform an instruction
 
-    print("Dynmic Instruction Count:", dic)
+    print("Dynmic Instruction Count: ", dic)
+    print("Single Cycle Count:       ", cycle[0])
+    print("Multi Cycle Count:        ", cycle[1])
+    print("Count 3 Step Cycles:      ", cycle[3])
+    print("Count 4 Step Cycles:      ", cycle[4])
+    print("Count 5 Step Cycles:      ", cycle[5])
+    print("Pipeline Cycle count:     " cycle[2]+=4)
